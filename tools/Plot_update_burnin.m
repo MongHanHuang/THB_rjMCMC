159588a2365f4bf68c3ea01c13abb9146d2ffd9e
MongHanHuang <52921307+MongHanHuang@users.noreply.github.com>
2021-12-27 10:03:15 -0500

    Add files via upload

---
+++ tools/Plot_update_burnin.m
@@ -0,0 +1,10 @@
+function Plot_update_burnin(folder_name,output_name,new_burnin)
+% Plot_update_burn(folder_name,output_name,new_burnin)
+% note: burn-in in percent of total iterations
+% will over-write the figures under "figures"
+
+load(['models/' folder_name '/' output_name '.mat']);
+
+burn = fix(new_burnin*length(ensemble));
+
+PlotEnsembleRef2D_verD
