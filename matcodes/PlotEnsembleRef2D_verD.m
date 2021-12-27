98df314616af81970ebed337f5fbc5fe3d98bfa3
MongHanHuang <52921307+MongHanHuang@users.noreply.github.com>
2021-12-27 09:59:15 -0500

    Update PlotEnsembleRef2D_verD.m

--- matcodes/PlotEnsembleRef2D_verD.m
+++ matcodes/PlotEnsembleRef2D_verD.m
@@ -2,12 +2,14 @@
 % Markov Chain Monte Carlo (rjMCMC) code for seismic refraction
 % 
 % Descreiption:
-%         Please find the user guide for more details
+%         Please find the user guide (THB2D MCMC User Guide v4.pdf) for more details
 %         
-% Please cite this code as: Huang, M.-H., Hudson-Rasmussen, B., Burdick, S., 
-% Lekic, V., Nelson, M.D., Fauria, K.E., and Schmerr, N., (2020), Bayesian 
-% seismic refraction inversion for critical zone science and near-surface 
-% applications, submitted to Geochem. Geophys. Geosys.
+% Please cite this code as: 
+% Huang, M.-H., Hudson-Rasmussen, B., Burdick, S., Lekic, V., Nelson, M.D.,
+%     Fauria, K.E., and Schmerr, N., (2020), Bayesian seismic refraction 
+%     inversion for critical zone science and near-surface applications, 
+%     Geopchemistry, Geophysics, Geosystems, 22, e2020GC009172.
+%     https://doi.org/10.1029/2020GC009172
 %     
 % Authors: Mong-Han Huang (mhhuang@umd.edu)
 %          Scott Burdick (sburdick@wayne.edu)
@@ -15,7 +17,10 @@
 %          Berit Hudson-Rasmussen (hudsonb@umd.edu)
 % 
 % Date: 11 Oct, 2020
-%        2 Mar, 2021 (add raypath density & coefficient of variations plots)
+%        2 Mar, 2021 (update input file names; enable FMM toolbox by Kroon 2021)
+%        1 Apr, 2021 (update noise hyperparameter allowing noise to vary with distance
+%       14 Sep, 2021 (enable different interpolation methods)
+%       19 Dec, 2021 (include raypath as an output in each iteration; add transparency to masked images)
 
 %%
 set(0,'defaultaxesfontsize',14)
@@ -23,9 +28,9 @@ set(0,'defaultaxesfontsize',14)
 % Add subdirectory to matlab path
 addpath(genpath('./matcodes/'))
 
-% Add fast marching toolbox location to matlab path
+% Add toolbox location to matlab path
 addpath(genpath('~/Documents/MATLAB/toolbox_fast_marching')) 
-addpath(genpath('~/Documents/MATLAB/FACS'))  % for scattered plot
+addpath(genpath('~/Documents/MATLAB/FACS'))  % for dscattered plot
 addpath(genpath('~/Documents/MATLAB/crameri_v1.05')) % for color scale
 
 figfolder = ['./models/' fname '/figures/'];
@@ -33,12 +38,31 @@ if ~exist(figfolder, 'dir')
     mkdir(figfolder);
 end
 
-%ipro = 1;
-%pronum = num2str(ipro); % this variable is captured in the "..._params.mat" file
-
-% Load ensemble and parameters
-load(['./models/' fname '/' fname '_Final_2D_ensemble.mat'],'X') % verD update to re-order the file loading sequence
 load(['./models/' fname '/' fname '_2D_grid.mat'],'Xg','Zg','ElevFMM')
+
+X = ensemble;
+
+%% plot misfit time series
+H=figure(1);clf;set(gca,'Fontsize',14);box on
+for i = 1:NumChain
+    tmp = [ENS{i}.E0]/Ndata; %misfit square
+    NNEN = length(ENS{i});
+    loglog((1:NNEN)*datsav,sqrt(tmp)*1e3,'linewidth',1.5); %plot in log scale
+    hold on;
+end
+xlabel('Iterations','Fontsize',14);
+ylabel('RMSE misfit (ms)','Fontsize',14);
+
+figfolder = ['./models/' fname '/figures/'];
+p1=3.5;
+set(H,'Units','Inches','Position',[1 1 1.5*p1 p1])
+imfile = sprintf('%s/MulChain_%s_RawMisfit.pdf',figfolder,pronum);
+set(gcf,'PaperPositionMode','auto')
+set(gcf,'Units','Inches', 'PaperSize', [1.5*p1 p1]);
+print(H,imfile,'-dpdf','-cmyk');
+
+imfile = sprintf('%s/MulChain_%s_RawMisfit.fig',figfolder,pronum);
+savefig(H,imfile,'compact');
 
 %%
 clear model
@@ -46,15 +70,18 @@ clear model
 X_tmp = X;
 X_tmp(1:burn) = [];
 E_tmp = [X_tmp.E0];
+raypath(1:burn) = [];
 
 % sort by E0 and only keep the top 90%
 [~,id] = sort(E_tmp);
 X0 = X_tmp(id(1:fix(length(id)*.9)));
+raypath0 = raypath(id(1:fix(length(id)*.9)));
 
 % Create error function, layer number vectors
 E = [X.E];
 E0 = [X.E0];
 xsig = [X.xsig];
+xsig2 = [X.xsig2];
 Nuclei = [X.Nuclei];
 
 Nens = length(E); % number of models in ensemble
@@ -66,7 +93,7 @@ Wvar = 0;
 dWmean_o = 0;
 dWvar = 0;
 
-% Colorbar with topo
+% Colorbar with topo (need "crameri")
 cc = [1 1 1; crameri('roma')];
 cstd = [1 1 1; crameri('lajolla')];
 ccc = [1 1 1;(parula(1024))];
@@ -83,7 +110,7 @@ for ii=1:length(X0)
     
     % Create new model
     F = scatteredInterpolant(model.zz',model.xx',model.v1D');
-    F.Method = 'linear';
+    F.Method = interpMethod;
     W = F(Zg,Xg);
     [dimz , dimx] = size(W);
     for i = 1:dimx
@@ -110,8 +137,12 @@ for ii=1:length(X0)
 
 end
 
+% rescale dWmean_o
+dWmean_o = dWmean_o * delta_X;
+
 % Create a polygon for area above ground surface
-polygonX = [Topo(:,1)', max(Topo(:,1))+delta_X/2, max(Topo(:,1))+delta_X/2, min(Topo(:,1))-delta_X/2, min(Topo(:,1))-delta_X/2];
+polygonX = [Topo(:,1)', max(Topo(:,1))+delta_X/2, max(Topo(:,1))+delta_X/2, ...
+    min(Topo(:,1))-delta_X/2, min(Topo(:,1))-delta_X/2];
 polygonY = max(Topo(:,2)) - Topo(:,2)';
 polygonY = [polygonY, polygonY(length(polygonY)), -delta_Z/2, -delta_Z/2, polygonY(1)];
 
@@ -250,7 +281,7 @@ print(H,imfile,'-dpdf','-cmyk');
 imfile = sprintf('%s/Travel_Time_Misfit_%s.fig',figfolder,pronum);
 savefig(H,imfile,'compact');
 
-%% plot raypath
+%% plot raypath of the mean velocity model
 H=figure(4);clf;set(gca,'Fontsize',14);box on
 
 X = Xg(1,:);
@@ -293,8 +324,14 @@ for ishot = 1:Nshot
     
     % Run FMM on reduced domain
     D = perform_fast_marching(Wmean, start_point, options);
-    
     gpath = compute_geodesic(D,end_points);
+    
+    %%% Kroon code (Don't use this)
+%     [~,D_Num] = size(ep2);
+%     D = msfm2d(Wmean, sp2, false, false);
+%     for i = 1:D_Num
+%         gpath{i} = shortestpath(D,ep2(:,i),sp2)';
+%     end
     
     hold on;
     
@@ -307,7 +344,6 @@ for ishot = 1:Nshot
         RayPath(M:M+N-1,1) = gpath{i}(2,:)*dX+minX;
         RayPath(M:M+N-1,2) = gpath{i}(1,:)*dX;
     end
-%      plot(start_point(2),start_point(1),'ro');
 end
 plot(Topo(:,1),max(Topo(:,2)) - Topo(:,2),'k','linewidth',2);
 axis image;
@@ -325,53 +361,26 @@ savefig(H,imfile,'compact');
 %% Estimate ray path density
 [y, x] = size(Wmean);
 dX = delta_X;
-ray_threshold = 1; % minimum ray path number
-ray_smooth = 5; % radius of a median fitler
+ray_smooth = 3; % radius of a median fitler
 RayDen0 = zeros(y,x);
-mask = zeros(y,x);
+% mask = zeros(y,x);
 
-MinX = min(RayPath(:,1))-dX;
-MinY = min(RayPath(:,2))-dX;
-
-for i = 1:length(RayPath)
-    tmp_x = round((RayPath(i,1)-MinX)/dX);
-    tmp_y = round((RayPath(i,2)-MinY)/dX);
-    if tmp_x < x && tmp_y < y
-        RayDen0(tmp_y,tmp_x) = RayDen0(tmp_y,tmp_x)+1;
-    end
+RayDensity = 0;
+for i = 1:length(raypath0)
+    RayDensity = RayDensity + raypath0(i).model;
 end
 
-for i = 1:x
-    for j = 1:y
-        if RayDen0(j,i) ~= 0
-            deep_ray(i,1) = (i-1)*dX+minX;
-            deep_ray(i,2) = (j+1)*dX;
-            mask(1:j,i) = 1;
-        end
-    end
-end
-
-mask(mask==0) = nan;
-
-RayDen = medfilt2(RayDen0,[ray_smooth ray_smooth]);
-RayDen(RayDen < ray_threshold) = nan;
-RayDen(isnan(RayDen)==0) = 1;
-
-H=figure(26);
-imagesc(Xg(1,:)',Z(1,:),log10(RayDen0));axis image;colormap([1 1 1;crameri('vik')]);
+H=figure(17);
+imagesc(Xg(1,:)',Z(1,:),log10(RayDensity));axis image;
+colormap([1 1 1;flipud(crameri('vik'))]);
 title(sprintf('Log ray path density %s',pronum),'Fontsize',14, 'Interpreter', 'none');
 xlabel('Distance (m)');ylabel('Depth (m)')
 
-imfile = sprintf('%s/RayPathDensity_%s.pdf',figfolder,pronum);
-p1=4.5;
+RayDen = medfilt2(RayDensity,[ray_smooth ray_smooth]);
+ray_threshold = max(max(RayDen))*.05; % minimum ray path number
 
-set(gcf,'PaperPositionMode','auto')
-set(H,'Units','Inches','Position',[1 1 1.5*p1 p1])
-set(gcf,'Units','Inches', 'PaperSize', [1.5*p1 p1]);
-print(H,imfile,'-dpdf','-cmyk');
-
-imfile = sprintf('%s/RayPathDensity_%s.fig',figfolder,pronum);
-savefig(H,imfile,'compact');
+RayDen(RayDen < ray_threshold) = nan;
+RayDen(isnan(RayDen)==0) = 1;
 
 % add one raw in the bottom of dWmean so the size matches
 [y,x] = size(dWmean_o);
@@ -393,7 +402,7 @@ plot(deep_ray(:,1),deep_ray(:,2),'w--','linewidth',2);
 title(sprintf('Mean velocity (m/s) %s',pronum),'Fontsize',14, 'Interpreter', 'none');
 xlabel('Distance (m)');ylabel('Depth (m)')
 
-imfile = sprintf('%s/Profile_%s_MeanVel.pdf',figfolder,pronum);
+imfile = sprintf('%s/%s_MeanVel.pdf',figfolder,pronum);
 p1=4.5;
 
 set(gcf,'PaperPositionMode','auto')
@@ -401,7 +410,7 @@ set(H,'Units','Inches','Position',[1 1 1.5*p1 p1])
 set(gcf,'Units','Inches', 'PaperSize', [1.5*p1 p1]);
 print(H,imfile,'-dpdf','-cmyk');
 
-imfile = sprintf('%s/Profile_%s_MeanVel.fig',figfolder,pronum);
+imfile = sprintf('%s/%s_MeanVel.fig',figfolder,pronum);
 savefig(H,imfile,'compact');
 
 %% Standard deviation of model
@@ -415,7 +424,7 @@ plot(deep_ray(:,1),deep_ray(:,2),'w--','linewidth',2);
 title(sprintf('STD in velocity (m/s)'),'Fontsize',14, 'Interpreter', 'none');
 xlabel('Distance (m)');ylabel('Depth (m)')
 
-imfile = sprintf('%s/Profile_%s_StdVel.pdf',figfolder,pronum);
+imfile = sprintf('%s/%s_StdVel.pdf',figfolder,pronum);
 p1=4.5;
 
 set(gcf,'PaperPositionMode','auto')
@@ -423,7 +432,7 @@ set(H,'Units','Inches','Position',[1 1 1.5*p1 p1])
 set(gcf,'Units','Inches', 'PaperSize', [1.5*p1 p1]);
 print(H,imfile,'-dpdf','-cmyk');
 
-imfile = sprintf('%s/Profile_%s_StdVel.fig',figfolder,pronum);
+imfile = sprintf('%s/%s_StdVel.fig',figfolder,pronum);
 savefig(H,imfile,'compact');
 
 %% coefficient of variance (std / mean)
@@ -438,7 +447,7 @@ plot(deep_ray(:,1),deep_ray(:,2),'w--','linewidth',2);
 title(sprintf('Coefficient of variance (percent)'),'Fontsize',14, 'Interpreter', 'none');
 xlabel('Distance (m)');ylabel('Depth (m)')
 
-imfile = sprintf('%s/Profile_%s_CoefVar.pdf',figfolder,pronum);
+imfile = sprintf('%s/%s_CoefVar.pdf',figfolder,pronum);
 p1=4.5;
 
 set(gcf,'PaperPositionMode','auto')
@@ -446,22 +455,22 @@ set(H,'Units','Inches','Position',[1 1 1.5*p1 p1])
 set(gcf,'Units','Inches', 'PaperSize', [1.5*p1 p1]);
 print(H,imfile,'-dpdf','-cmyk');
 
-imfile = sprintf('%s/Profile_%s_CoefVar.fig',figfolder,pronum);
+imfile = sprintf('%s/%s_CoefVar.fig',figfolder,pronum);
 savefig(H,imfile,'compact');
 
 %% Mean vertical gradient (i.e. strength of interface)
 H=figure(7);clf;
 imagesc(Xg(1,:)',Z(1,:),dWmean);daspect([1 1 1]);
 colorbar;colormap(ccc);
-% caxis([0 max(max(dWmean))*0.4]);
-caxis([-100 200]);
+caxis([-100 max(max(dWmean))*0.5]);
+% caxis([-100 200]);
 hold on;patch(polygonX,polygonY,'w');
 plot(Topo(:,1),max(Topo(:,2)) - Topo(:,2),'k','linewidth',2);
 plot(deep_ray(:,1),deep_ray(:,2),'w--','linewidth',2);
-title(sprintf('Mean vertical gradient (m/s) %s',pronum),'Fontsize',14, 'Interpreter', 'none');
+title(sprintf('Mean vertical gradient (m/s/m) %s',pronum),'Fontsize',14, 'Interpreter', 'none');
 xlabel('Distance (m)');ylabel('Depth (m)')
 
-imfile = sprintf('%s/Profile_%s_MeanGrad.pdf',figfolder,pronum);
+imfile = sprintf('%s/%s_MeanGrad.pdf',figfolder,pronum);
 p1=4.5;
 
 set(gcf,'PaperPositionMode','auto')
@@ -469,11 +478,11 @@ set(H,'Units','Inches','Position',[1 1 1.5*p1 p1])
 set(gcf,'Units','Inches', 'PaperSize', [1.5*p1 p1]);
 print(H,imfile,'-dpdf','-cmyk');
 
-imfile = sprintf('%s/Profile_%s_MeanGrad.fig',figfolder,pronum);
+imfile = sprintf('%s/%s_MeanGrad.fig',figfolder,pronum);
 savefig(H,imfile,'compact');
 
 %% Masked Mean model (crop out regions where CoV < a threshold)
-CoV_threshold = 40; % percent
+CoV_threshold = 50; % percent
 
 mask_CoV = CoV;
 mask_CoV(mask_CoV < CoV_threshold) = 1;
@@ -487,11 +496,14 @@ for i = 1:x
     end
 end
 
+mask_Wmean2 = Wmean .* mask_CoV .* mask_offset .* RayDen;
+
 H=figure(8);clf;
-imagesc(Xg(1,:)',Z,mask_Wmean .* mask_CoV .* mask_offset);daspect([1 1 1]);
+imagesc(Xg(1,:)',Z,Wmean);daspect([1 1 1]);hold on
+imagesc(Xg(1,:)',Z,mask_Wmean2,'AlphaData',.6);daspect([1 1 1]);
 colormap(cc);colorbar;
 caxis([0 4000]);
-hold on;patch(polygonX,polygonY,'w');
+patch(polygonX,polygonY,'w');
 plot(Topo(:,1),max(Topo(:,2)) - Topo(:,2),'k','linewidth',2);
 title(sprintf('Masked Mean velocity (m/s) %s',pronum),'Fontsize',14, 'Interpreter', 'none');
 xlabel('Distance (m)');ylabel('Depth (m)')
@@ -509,7 +521,7 @@ savefig(H,imfile,'compact');
 
 %% Interpolate masked mean model
 [x,y] = size(mask_Wmean);
-mask_Wmean2 = mask_Wmean .* mask_CoV .* mask_offset;
+% mask_Wmean2 = mask_Wmean .* mask_CoV .* mask_offset;
 S = mask_Wmean2(:);
 nanS = find(isnan(mask_Wmean2)==0);
 S_s = S(nanS);
@@ -519,9 +531,9 @@ Yc = yi(:);
 Xs = Xc(nanS);
 Ys = Yc(nanS);
 mask_W_intp = griddata(Xs,Ys,S_s,xi,yi,'cubic');
-mask_W_intp = mask_W_intp .* mask .* mask_offset;
+mask_W_intp = mask_W_intp .* mask_offset;
 
-H=figure(25);clf;
+H=figure(9);clf;
 imagesc(Xg(1,:)',Z,mask_W_intp);daspect([1 1 1]);
 colormap(cc);colorbar;
 caxis([0 4000]);
@@ -544,13 +556,14 @@ savefig(H,imfile,'compact');
 %% Masked Mean vertical gradient (i.e. strength of interface)
 
 H=figure(10);clf;
-imagesc(Xg(1,:)',Z(1,:),mask_dWmean);daspect([1 1 1]);
+imagesc(Xg(1,:)',Z(1,:),dWmean);hold on
+imagesc(Xg(1,:)',Z(1,:),dWmean .* mask_CoV .* mask_offset .* RayDen,'AlphaData',.5);
+daspect([1 1 1]);
 colorbar;colormap(ccc);
-% caxis([0 max(max(dWmean))*0.4]);
-caxis([-100 300]);
+caxis([-100 max(max(dWmean))*0.5]);
 hold on;patch(polygonX,polygonY,'w');
 plot(Topo(:,1),max(Topo(:,2)) - Topo(:,2),'k','linewidth',2);
-title(sprintf('Mean vertical gradient (m/s) Profile %s',pronum),'Fontsize',14, 'Interpreter', 'none');
+title(sprintf('Masked mean vertical gradient (m/s/m) %s',pronum),'Fontsize',14, 'Interpreter', 'none');
 xlabel('Distance (m)');ylabel('Depth (m)')
 
 imfile = sprintf('%s/Masked_%s_MeanGrad.pdf',figfolder,pronum);
@@ -573,27 +586,33 @@ xlabel('Control points','Fontsize',14);ylabel('Frequency','Fontsize',14)
 
 p1=4.5;
 set(H,'Units','Inches','Position',[1 1 1.1*p1 p1])
-imfile = sprintf('%s/Profile_%s_NumLayers.pdf',figfolder,pronum);
+imfile = sprintf('%s/%s_NumLayers.pdf',figfolder,pronum);
 set(gcf,'PaperPositionMode','auto')
 set(gcf,'Units','Inches', 'PaperSize', [1.1*p1 p1]);
 print(H,imfile,'-dpdf','-cmyk');
-imfile = sprintf('%s/Profile_%s_NumLayers.fig',figfolder,pronum);
+imfile = sprintf('%s/%s_NumLayers.fig',figfolder,pronum);
 savefig(H,imfile,'compact');
 
+%% plot both noise and noise slope
 H=figure(12);clf;set(gca,'Fontsize',14);box on
+subplot(1,2,1);
 % convert from natural log to 10 based
-xsig10 = log10(exp(xsig));
-hist(xsig10(burn:Nens-1),50)
-title(sprintf('Noise hyperparameter, N=%d, Profile %s',cnt,pronum),'Fontsize',14, 'Interpreter', 'none')
-xlabel('Log seconds','Fontsize',14);ylabel('Frequency','Fontsize',14)
+% hist(xsig10(burn:Nens-1),50);
+hist(exp(xsig(burn:Nens-1))*1e3,50);
+title(sprintf('Noise hyperparameter, N=%d,\n %s',cnt,pronum),'Fontsize',14, 'Interpreter', 'none')
+xlabel('ms','Fontsize',14);ylabel('Frequency','Fontsize',14)
+subplot(1,2,2);
+hist(xsig2(burn:Nens-1)*1e3,50)
+title(sprintf('Noise slope with distance,\n %s',pronum'),'Fontsize',14, 'Interpreter', 'none')
+xlabel('ms per meter','Fontsize',14);ylabel('Frequency','Fontsize',14)
 
 p1=4.5;
-set(H,'Units','Inches','Position',[1 1 1.1*p1 p1])
-imfile = sprintf('%s/Profile_%s_NoiseVar.pdf',figfolder,pronum);
+set(H,'Units','Inches','Position',[1 1 2.1*p1 p1])
+imfile = sprintf('%s/%s_NoiseVar.pdf',figfolder,pronum);
 set(gcf,'PaperPositionMode','auto')
-set(gcf,'Units','Inches', 'PaperSize', [1.1*p1 p1]);
+set(gcf,'Units','Inches', 'PaperSize', [2.1*p1 p1]);
 print(H,imfile,'-dpdf','-cmyk');
-imfile = sprintf('%s/Profile_%s_NoiseVar.fig',figfolder,pronum);
+imfile = sprintf('%s/%s_NoiseVar.fig',figfolder,pronum);
 savefig(H,imfile,'compact');
 
 %% Raw misfit function evolution (combine all chains)
@@ -605,12 +624,12 @@ xlabel('Saved iteration','Fontsize',14);ylabel('RMSE misfit (ms)','Fontsize',14)
 
 p1=3.5;
 set(H,'Units','Inches','Position',[1 1 1.5*p1 p1])
-imfile = sprintf('%s/Profile_%s_RawMisfit.pdf',figfolder,pronum);
+imfile = sprintf('%s/%s_RawMisfit.pdf',figfolder,pronum);
 set(gcf,'PaperPositionMode','auto')
 set(gcf,'Units','Inches', 'PaperSize', [1.5*p1 p1]);
 print(H,imfile,'-dpdf','-cmyk');
 
-imfile = sprintf('%s/Profile_%s_RawMisfit.fig',figfolder,pronum);
+imfile = sprintf('%s/%s_RawMisfit.fig',figfolder,pronum);
 savefig(H,imfile,'compact');
 
 %% Noise hyperparameter evolution (combine all chains)
@@ -622,12 +641,12 @@ xlabel('Iteration','Fontsize',14);ylabel('Noise hyp. (ms)','Fontsize',14)
 
 p1=3.5;
 set(H,'Units','Inches','Position',[1 1 1.5*p1 p1])
-imfile = sprintf('%s/Profile_%s_NoiseEvol.pdf',figfolder,pronum);
+imfile = sprintf('%s/%s_NoiseEvol.pdf',figfolder,pronum);
 set(gcf,'PaperPositionMode','auto')
 set(gcf,'Units','Inches', 'PaperSize', [1.5*p1 p1]);
 print(H,imfile,'-dpdf','-cmyk');
 
-imfile = sprintf('%s/Profile_%s_NoiseEvol.fig',figfolder,pronum);
+imfile = sprintf('%s/%s_NoiseEvol.fig',figfolder,pronum);
 savefig(H,imfile,'compact');
 
 %% Misfit function evolution (combine all chains in log scale)
@@ -639,10 +658,17 @@ xlabel('Saved iteration','Fontsize',14);ylabel('Chi^2','Fontsize',14)
 
 p1=3.5;
 set(H,'Units','Inches','Position',[1 1 1.5*p1 p1])
-imfile = sprintf('%s/Profile_%s_Misfit.pdf',figfolder,pronum);
+imfile = sprintf('%s/%s_Misfit.pdf',figfolder,pronum);
 set(gcf,'PaperPositionMode','auto')
 set(gcf,'Units','Inches', 'PaperSize', [1.5*p1 p1]);
 print(H,imfile,'-dpdf','-cmyk');
 
-imfile = sprintf('%s/Profile_%s_Misfit.fig',figfolder,pronum);
+imfile = sprintf('%s/%s_Misfit.fig',figfolder,pronum);
 savefig(H,imfile,'compact');
+
+%% save the figure products
+save([figfolder '/' pronum '_fig.mat'],'mask_CoV','Wmean','dWmean',...
+    'mask_Wmean','mask_Wmean2','mask_offset','mask_dWmean','CoV',...
+    'Xg','Z','polygonX','polygonY','Topo','RayDen','figfolder','dX',...
+    'ElevFMM','mask_W_intp','delta_X','delta_Z','pronum','deep_ray',...
+    'Wvar','cnt','fname','mask_W_intp','Master');
